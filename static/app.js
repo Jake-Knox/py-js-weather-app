ec9c62e62db9b2e9e0e754a4cad3f3622d8eb034
jake-knox- <jakeknox11@hotmail.co.uk>
2023-06-30 12:06:31 +0100

    small fixes, start of CRUD

--- static/app.js
+++ static/app.js
@@ -23,7 +23,7 @@ searchButton.addEventListener("click", () => {
     .then(response => response.json())
     .then(data => {
 
-      console.log(data);
+      // console.log(data);
 
       // process data from the backend
       let temperature = data.temperature;
